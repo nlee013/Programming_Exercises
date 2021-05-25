7c7b2cbf2b19fb0c86abe937443e30c817984be4
nlee013 <83215027+nlee013@users.noreply.github.com>
2021-05-25 18:17:10 +0900

    add

---
+++ 0525_Exercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0525_Exercise4
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            const int N = 1000;
+
+            int sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                sum += CoinFlip() ? 1 : 0;
+            }
+            Console.WriteLine(double)sum/N);
+
+        }
+        static bool CoinFlip()
+        {
+
+        }
+    }
+}
